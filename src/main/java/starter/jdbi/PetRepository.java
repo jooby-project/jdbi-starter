119f21050ffebbb70b02246d0e691ebb601fe220
Rustam Ismailov <ri@cashup.com.ua>
2018-10-10 23:20:07 +0400

    Added Gradle project

--- src/main/java/starter/jdbi/PetRepository.java
+++ src/main/java/starter/jdbi/PetRepository.java
@@ -13,50 +13,50 @@ import java.util.List;
  */
 @RegisterRowMapper(Pet.Mapper.class)
 public interface PetRepository {
-  /**
-   * List pets using start/max limits.
-   *
-   * @param start Start offset.
-   * @param max Max number of rows.
-   * @return Available pets.
-   */
-  @SqlQuery("select * from pets limit :max offset :start")
-  List<Pet> list(int start, int max);
+    /**
+     * List pets using start/max limits.
+     *
+     * @param start Start offset.
+     * @param max Max number of rows.
+     * @return Available pets.
+     */
+    @SqlQuery("select * from pets limit :max offset :start")
+    List<Pet> list(int start, int max);
 
-  /**
-   * Find a pet by ID.
-   *
-   * @param id Pet ID.
-   * @return Pet or null.
-   */
-  @SqlQuery("select * from pets where id=:id")
-  Pet findById(long id);
+    /**
+     * Find a pet by ID.
+     *
+     * @param id Pet ID.
+     * @return Pet or null.
+     */
+    @SqlQuery("select * from pets where id=:id")
+    Pet findById(long id);
 
-  /**
-   * Insert a pet and returns the generated PK.
-   *
-   * @param pet Pet to insert.
-   * @return Primary key.
-   */
-  @SqlUpdate("insert into pets(name) values(:name)")
-  @GetGeneratedKeys
-  long insert(@BindBean Pet pet);
+    /**
+     * Insert a pet and returns the generated PK.
+     *
+     * @param pet Pet to insert.
+     * @return Primary key.
+     */
+    @SqlUpdate("insert into pets(name) values(:name)")
+    @GetGeneratedKeys
+    long insert(@BindBean Pet pet);
 
-  /**
-   * Update a pet and returns true if the pets was updated.
-   *
-   * @param pet Pet to update.
-   * @return True if the pet was updated.
-   */
-  @SqlUpdate("update pets set name=:name where id=:id")
-  boolean update(@BindBean Pet pet);
+    /**
+     * Update a pet and returns true if the pets was updated.
+     *
+     * @param pet Pet to update.
+     * @return True if the pet was updated.
+     */
+    @SqlUpdate("update pets set name=:name where id=:id")
+    boolean update(@BindBean Pet pet);
 
-  /**
-   * Delete a pet by ID.
-   *
-   * @param id Pet ID.
-   * @return True if the pet was deleted.
-   */
-  @SqlUpdate("delete pets where id=:id")
-  boolean delete(long id);
+    /**
+     * Delete a pet by ID.
+     *
+     * @param id Pet ID.
+     * @return True if the pet was deleted.
+     */
+    @SqlUpdate("delete pets where id=:id")
+    boolean delete(long id);
 }
